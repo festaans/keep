64e7c5a7ffd6719db70b0ca3a7db4a1298119853
yauvanDestroyer <yhansraj@deloitte.co.za>
2015-07-07 11:24:44 +0200

    basic structure

---
+++ dev/components/views/yauvansKeep/yauvan.controller.js
@@ -0,0 +1,13 @@
+app.controller('yauvanController',['$scope', 'loadUser', 'loadItems', function($scope, loadUser, loadItems) {
+  $scope.user = "";
+  $scope.items = "";
+
+  loadUser.success(function(data) {
+      $scope.user = data.user;
+  });
+
+  loadItems.success(function(data) {
+      $scope.items = data.items;
+  });
+
+}]);
---
+++ dev/components/views/yauvansKeep/yauvan.state.js
@@ -0,0 +1,5 @@
+.state('yauvan', {
+  url: "/yauvan",
+  templateUrl: "components/views/yauvansKeep/yauvan.html",
+  controller: "yauvanController"
+})  //do not add a ;
