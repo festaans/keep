c936bb232a8d6e55625ef25f2140424e2ab7e36c
mpaduri <mpaduri@deloitte.co.za>
2015-07-08 09:56:23 +0200

    feature/m_html

--- dev/components/views/manoj/manoj.controller.js
+++ dev/components/views/manoj/manoj.controller.js
@@ -1,6 +1,17 @@
-app.controller('manojController',['$scope', 'loadUser', 'loadArticles', function($scope, loadUser, loadArticles) {
+app.controller('manojController',['$scope', 'loadUser', 'loadItems', function($scope, loadUser, loadItems) {
 
   //set doc title
   window.document.title = 'UI Challenge 2';
+
+  $scope.user = "";
+  $scope.items = "";
+
+  loadUser.success(function(data) {
+      $scope.user = data.user;
+  });
+
+  loadItems.success(function(data) {
+      $scope.items = data.items;
+  });
 
 }]);
